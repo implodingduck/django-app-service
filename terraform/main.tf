d0f7022c4456f53e99ebe602c476a041521811f3
implodingduck <zsek320@gmail.com>
2022-07-22 16:15:35 -0500

    adding app service stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.83.0"
+      version = "=3..0"
     }
     random = {
       source  = "hashicorp/random"
@@ -53,4 +53,42 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-${local.gh_repo}-${random_string.unique.result}-${local.loc_for_naming}"
   location = var.location
   tags = local.tags
+}
+
+resource "azurerm_application_insights" "app" {
+  name                = "${local.gh_repo}-${random_string.unique.result}-insights"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "other"
+  workspace_id        = data.azurerm_log_analytics_workspace.default.id
+}
+
+
+resource "azurerm_service_plan" "asp" {
+  name                = "asp-${local.gh_repo}-${random_string.unique.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "${local.gh_repo}${random_string.unique.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.asp.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  app_settings {
+    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.app.instrumentation_key
+  }
+  application_stack {
+    python_version = 3.9
+  }
+  site_config {
+
+  }
+
+  identity{
+    type = "SystemAssigned"
+  }
 }
