4a9f9488f258968eafa0bfa09f0c66a06c66f34d
implodingduck <zsek320@gmail.com>
2022-08-29 11:32:58 -0500

    adjusting static root

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'mysite', 'static')
 
 
 # Default primary key field type
