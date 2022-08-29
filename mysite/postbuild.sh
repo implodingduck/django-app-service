33c1f88406420a8cd1c031bc127adb5a46617f42
implodingduck <zsek320@gmail.com>
2022-08-29 11:46:18 -0500

    adjusting postbuild

--- mysite/postbuild.sh
+++ mysite/postbuild.sh
@@ -2,6 +2,13 @@
 echo "============================="
 echo "Perform database migration..."
 pip freeze
+ls -lta /opt
+ls -lta /opt/microsoft
+ls -lta /opt/microsoft/msodbcsql17/lib64/
+pip install pyodbc==4.0.34
+ls -lta /opt
+ls -lta /opt/microsoft
+ls -lta /opt/microsoft/msodbcsql17/lib64/
 python manage.py migrate
 echo "Finished database migration..."
 echo "============================="
