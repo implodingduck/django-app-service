00ff392b7fc8724db477b8be794e94ee96d0ec0c
implodingduck <zsek320@gmail.com>
2022-08-29 13:22:11 -0500

    updating the custom startup script

--- mysite/customstartup.sh
+++ mysite/customstartup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-
+echo "-====-"
+echo "Migrating DB..."
 python manage migrate
+echo "Migration done..."
+echo "-====-"
+echo "Starting gunicorn..."
 gunicorn --bind=0.0.0.0 --timeout 600 mysite.wsgi
